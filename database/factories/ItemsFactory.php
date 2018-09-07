bab73abd2c4ae289dd492919aaaa3f8ee39b49b4
ottowao <pat@patmando.com>
2018-09-07 11:26:44 +1000

    Items factory

---
+++ database/factories/ItemsFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(App\Item::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'description' => $faker->sentence,
+        'stock' => mt_rand(1, 100),
+        'cost' => (mt_rand(1*10, 100*10) / 10),
+        'created_at' => $faker->dateTimeThisYear,
+    ];
+});
